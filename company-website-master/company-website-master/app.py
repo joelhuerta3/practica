b5746178b8933e34abd5b9d130063252b43f86b5
joelhuerta3 <161348464+joelhuerta3@users.noreply.github.com>
2025-02-20 13:54:02 -0600

    Update app.py

--- company-website-master/company-website-master/app.py
+++ company-website-master/company-website-master/app.py
@@ -13,8 +13,8 @@ def load_lottieurl(url):
         return None
     return r.json()
 
-def local_css(file_name):
-    with open(file_name) as f:
+def local_css(style.css):
+    with open(style.css) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 local_css("style/style.css")
